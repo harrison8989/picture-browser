0f2fd7d6c08b9e92a9adb881404a10b62a1e5b6d
Harrison Ho <harrison8989@gmail.com>
2015-03-22 04:29:19 -0700

    Can now switch between different folders using dropdown select menu

--- picture-browser.js
+++ picture-browser.js
@@ -6,19 +6,20 @@
 	var folder = '';
 	var photos = [];
 	var pictureElem = document.getElementById("picture-browser");
+	var selectWrapperElem;
+	var photoElem;
+	var listElem;
 	var metaFile = "files.txt";
 	var photoURL = "";
-
-	//Set up event listeners
-	function addEvents() {
-
-	}
 
 	//Get photos from the directory
 	function getPhotos(callback) {	
 		var requestObj = new XMLHttpRequest();
 		requestObj.addEventListener("load", function() {
 			var rawPhotos = requestObj.responseText.split('\n');
+
+			//convert raw to new photos
+			photos = []
 			for(var i = 0; i < rawPhotos.length; i++) {
 				if(rawPhotos[i].length > 0)
 					photos.push(rawPhotos[i]);
@@ -34,11 +35,9 @@
 		return "<img src=\"" + folder + "/" + photo + "\">";
 	}
 
-	//Add photos to the elements
-	function addPhotos() {
+	//Add photos to the elements. Used as a callback function
+	function generateBrowserStructure() {
 		if(photos.length > 0) {
-			var photoElem = document.getElementById("picture-browser-photo");
-			var listElem = document.getElementById("picture-browser-list");
 			photoElem.innerHTML = formatIMGURL(photos[0]);
 
 			var ulElem = document.createElement("ul");
@@ -56,39 +55,63 @@
 				ulElem.appendChild(liElem);
 			});
 			listElem.appendChild(ulElem);
-
-			//add event listeners after photos have been added
-			addEvents(photoElem, listElem);
 		} else {
 			pictureElem.innerHTML = "<p>No images were found!</p>";
 		}
 	}
 
 	//Display the photos contained in one folder
-	function displayPhotos(folderID) {
-		folderID = folderID || 0;
-		folder = folders[folderID];
-		pictureElem.innerHTML = "";
+	function displayPhotos(targetFolder) {
+		folder = targetFolder || folders[0];
 
-		//create elements
-		var photoElem = document.createElement("div");
-		photoElem.setAttribute("id", "picture-browser-photo");
-		var listElem = document.createElement("div");
-		listElem.setAttribute("id", "picture-browser-list");
-		pictureElem.appendChild(photoElem);
-		pictureElem.appendChild(listElem);
+		//reset divs
+		photoElem.innerHTML = "";
+		listElem.innerHTML = "";
 
 		//add images
-		var photos = getPhotos(addPhotos);
+		var photos = getPhotos(generateBrowserStructure);
+	}
+
+	//Create elements of document
+	function initElements() {
+		//select
+		selectWrapperElem = document.createElement("div");
+		selectWrapperElem.setAttribute("id", "picture-browser-select");
+		var selectElem = document.createElement("select");
+		for(var i = 0; i < folders.length; i++) {
+			var optionElem = document.createElement("option");
+			optionElem.setAttribute("value", folders[i]);
+			optionElem.innerHTML = folders[i];
+			selectElem.appendChild(optionElem);
+		}
+		selectWrapperElem.appendChild(selectElem);
+
+		//add event handler for select
+		selectElem.onchange = function(event) {
+			displayPhotos(selectElem.value);
+			return true;
+		}
+
+		//photo and list
+		photoElem = document.createElement("div");
+		photoElem.setAttribute("id", "picture-browser-photo");
+		listElem = document.createElement("div");
+		listElem.setAttribute("id", "picture-browser-list");
+
+		//Actually add elements
+		pictureElem.appendChild(selectWrapperElem);
+		pictureElem.appendChild(photoElem);
+		pictureElem.appendChild(listElem);
 	}
 
 	//Grab meta data from the html file to use folders
 	function initMetas() {
-		var metas = document.getElementsByTagName('meta');
+		var metas = document.getElementsByTagName("meta");
 		var folderString = '';
 		for(var i = 0; i < metas.length; i++) {
 			if(metas[i].getAttribute("name") === "picture-browser") {
 				folderString = metas[i].getAttribute("content");
+				break;
 			}
 		}
 		if(folderString !== '') {
@@ -98,8 +121,10 @@
 			}
 
 			//display photos
-			if(folders.length > 0)
+			if(folders.length > 0) {
+				initElements();
 				displayPhotos();
+			}
 		}
 	};
 
